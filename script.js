d63e9e196cebd571b78ba5c481713e73b2520989
myatthwin <mgmyatthwinlinn@gmail.com>
2022-12-10 00:50:54 +0630

    ES6 properties

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
- const x = 12;
- x = 2;
- console.log(x);
+//ES6 map,filter,reduce
+
+let nums = [1, 2, 3, 4, 5]; //in js functin map
+let result = nums.map(function(n) {
+    return n + 1;
+})
+console.log(result);
+
+let mapfun = [1, 2, 3, 4]; //in arrow map funcrion
+let mapResult = mapfun.map(n => n + 1);
+console.log(mapResult);
+
+let filNum = [1, 2, 3, 4, 5]; //in js filter function
+let filResult = filNum.filter(function(n) {
+    return n % 2;
+})
+console.log(filResult);
+
+let arrFilter = [1, 2, 3, 4, 5]; //in arrow filter function
+let arrResult = arrFilter.filter(n => n % 2);
+console.log(arrResult);
